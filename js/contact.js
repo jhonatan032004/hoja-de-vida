a55ebefec924951236a1815075b7a33f79bb6a38
jhonatan032004 <jpalacio1137@gmail.com>
2022-09-27 22:24:27 -0500

    problemas

---
+++ js/contact.js
@@ -0,0 +1,11 @@
+let cbtn = document.getElementById('correobtn');
+
+let divc = document.getElementById('correo')
+
+cbtn.addEventListener('click', (e) => {
+    
+
+    divc.innerHTML = `<div id="correo">jpalacio172@misena.edu.co</div>
+    `
+    console.log("siu")
+})
