b35d24c2a388c4977dabb4ac1006abed9a4f82e5
jhonatan032004 <jpalacio1137@gmail.com>
2022-09-27 22:39:35 -0500

    solucion problema -responsive

--- js/contact.js
+++ js/contact.js
@@ -1,11 +1,35 @@
 let cbtn = document.getElementById('correobtn');
-
-let divc = document.getElementById('correo')
+let divc = document.getElementById('correo');
 
 cbtn.addEventListener('click', (e) => {
-    
+    divc.innerHTML = `<div id="correos">jpalacio172@misena.edu.co</div>
+    <style> #correos{float: left;
+        margin-left: 25px;
+        height: 20px;
+        padding-top: 10px;
+        width: 600px;
+        color:white;
+        border-radius: 10px;
+        background-color: #245970;
+        text-align: center;
+        margin-bottom: 20px;}</style>`
+    console.log("btn correo presionado")
+})
 
-    divc.innerHTML = `<div id="correo">jpalacio172@misena.edu.co</div>
-    `
-    console.log("siu")
+let cebtn = document.getElementById('celularbtn');
+let divce = document.getElementById('celular');
+
+cebtn.addEventListener('click', (e) => {
+    divce.innerHTML = `<div id="celulard">+57 3194606488</div>
+    <style> #celulard{float: left;
+        margin-left: 25px;
+        height: 20px;
+        padding-top: 10px;
+        width: 600px;
+        color:white;
+        border-radius: 10px;
+        background-color: #245970;
+        text-align: center;
+        margin-bottom: 20px;}</style>`
+    console.log("btn correo presionado")
 })
